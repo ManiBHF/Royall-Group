5dd84fc1a231d82aa9ad08b994a2369e074a979c
Mani Behforouzi <manibeh84@gmail.com>
2023-03-01 13:11:32 +0330

    new files added to my working directory

---
+++ Ramtin's Project/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    colors: {
+    'main-color' : "#c9ae79",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
