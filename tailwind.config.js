d79bfd3298f52b41deb10d1e103685461063df65
Ramtin Behforouzi <behforouzir@gmail.com>
2023-03-02 17:02:46 +0330

    add font dana

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-    'main-color' : "#c9ae79",
+    container: {
+      center: true,
+      padding: '1rem',
     },
-    extend: {},
+    fontFamily: {
+      'danawt': 'danawt'
+    },
+    extend: {
+      boxShadow: {
+        'mobile-menu': '0 0px 10px 0px rgba(0, 0, 0, .14);',
+      }
+    },
   },
   plugins: [],
 }
