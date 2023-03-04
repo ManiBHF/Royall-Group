2ff76beb1e901c2e36e630091a33de3ee9af50e7
Ramtin Behforouzi <behforouzir@gmail.com>
2023-03-05 00:57:17 +0330

    add new project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    fontFamily: {
+      'danawt': 'danawt'
+    },
+    extend: {
+      boxShadow: {
+        'mobile-menu': '0 0px 10px 0px rgba(0, 0, 0, .14);',
+      }
+    },
   },
   plugins: [],
 }
